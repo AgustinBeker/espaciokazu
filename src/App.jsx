1f51a3032136004a0c0838d7cdda069c6b9e3ff6
AgustinBeker <agustin2012beker@gmail.com>
2023-05-30 10:02:13 -0300

    Footer add

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import {BrowserRouter, Routes, Route } from 'react-router-dom'
 import NavBar from './components/NavBar/NavBar';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
+import { Footer } from './components/Footer/Footer';
 
 function App() {
   return (
@@ -14,7 +15,8 @@ function App() {
           <Route path='/category/:categoryId' element={<ItemListContainer/>} />
           <Route path='/item/:itemId' element={<ItemDetailContainer/>} />
           <Route path='*' element={<h1>404 NOT FOUND</h1>} />
-        </Routes>      
+        </Routes>
+        <Footer/>      
       </BrowserRouter>
     </div>
   );
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,10 @@
+import './Footer.css'
+import React from 'react'
+
+export const Footer = () => {
+    return (
+    <div className="footer">
+        <p>Designed & Developed by Agustin Beker - 2023</p>
+    </div>
+    );
+};
